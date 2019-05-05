e87b2d7fc15cb608bd8c2d808b9670b870f0b01e
Manikandan Natarajan <manikann@gmail.com>
2019-05-05 15:07:17 +0800

    chart from jfrog; before customization

---
+++ terraform/gcs-artifactory-config.tf
@@ -0,0 +1,25 @@
+resource "google_storage_bucket" "artifactory_config" {
+  project       = "${var.project_id}"
+  name          = "${var.project_id}-artifactory-config"
+  location      = "${var.region}"
+  storage_class = "REGIONAL"
+  force_destroy = "true"
+}
+
+resource "google_storage_bucket_object" "copy-master-key" {
+  bucket = "${google_storage_bucket.artifactory_config.id}"
+  name   = "master.key"
+  source = "master.key"
+}
+
+resource "google_storage_bucket_object" "db-password" {
+  bucket = "${google_storage_bucket.artifactory_config.id}"
+  name   = "db.password"
+  source = "db.password"
+}
+
+resource "google_storage_bucket_object" "access-password" {
+  bucket = "${google_storage_bucket.artifactory_config.id}"
+  name   = "access.password"
+  source = "access.password"
+}
