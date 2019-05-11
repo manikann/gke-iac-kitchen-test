bb6bb31683d368abbd7ae6652c795a506414c2bc
Manikandan Natarajan <manikann@gmail.com>
2019-05-11 21:04:52 +0800

    Artifactory configuration automation

--- install/setupRepo.sh
+++ install/setupRepo.sh
@@ -11,6 +11,7 @@ declare -A OPTS=(
             ["edge-baseurl:"]="Base URL of the artifactory edge"
            ["edge-useridid:"]="Edge artifactory userid"
            ["edge-password:"]="Edge artifactory password"
+            ["edge-replurl:"]="Base URL of the artifactory edge for replication"
       )
 
 # CLI parameters are
@@ -18,6 +19,7 @@ _MASTER_BASEURL=""
 _MASTER_USERID=""
 _MASTER_PWD=""
 _EDGE_BASEURL=""
+_EDGE_REPLURL=""
 _EDGE_USERID=""
 _EDGE_PWD=""
 MASTER_ACCESS_TOKEN=""
@@ -32,7 +34,7 @@ function usage() {
   done
   echo
   echo "example"
-  echo "./setupRepo.sh --name jcenter --remote-url 'https://jcenter.bintray.com' --master-baseurl 'http://localhost:8081/artifactory' --master-password 'Welcome!23' --edge-baseurl 'http://localhost:8082/artifactory'"
+  echo "./setupRepo.sh --master-baseurl 'http://localhost:8081/artifactory' --master-password 'Welcome!23' --edge-baseurl 'http://localhost:8082/artifactory'"
   exit 1
 }
 
@@ -55,6 +57,7 @@ function parseArguments() {
                  --master-userid) _MASTER_USERID=$2; shift 2;;
                --master-password) _MASTER_PWD=$2; shift 2;;
                   --edge-baseurl) _EDGE_BASEURL=$2; shift 2;;
+                  --edge-replurl) _EDGE_REPLURL=$2; shift 2;;
                    --edge-userid) _EDGE_USERID=$2; shift 2;;
                  --edge-password) _EDGE_PWD=$2; shift 2;;
                               --) shift ; break ;;
@@ -70,6 +73,7 @@ parseArguments $@
 [[ -z "$_MASTER_USERID" ]] && _MASTER_USERID="admin"
 [[ -z "$_EDGE_USERID" ]] && _EDGE_USERID="admin"
 [[ -z "$_EDGE_PWD" ]] && _EDGE_PWD=${_MASTER_PWD}
+[[ -z "$_EDGE_REPLURL" ]] && _EDGE_REPLURL=${_EDGE_BASEURL}
 
 # Validate mandatory input
 [[ -z "$_MASTER_BASEURL" || \
@@ -119,8 +123,7 @@ function _curl() {
   shift 2
   local token=$(getConfig $site token)
   local baseurl=$(getConfig $site baseurl)
-  set -x
-  curl -ksSLf -v \
+  curl -ksSLf \
        -H 'Content-Type: application/json' \
        -H "Authorization: Bearer ${token}" \
        ${baseurl}/${uri} \
@@ -132,7 +135,7 @@ function isRepoExist() {
   local repoName=$2
   local repoJson=$(_curl $site api/repositories/${repoName} -XGET 2>/dev/null)
   if [[ ! -z "$repoJson" ]]; then
-    echo "repo ${repoName} already exists at ${site}"
+    echo "Repo ${repoName} already exists at ${site}"
     return 0
   fi
   return 1
@@ -145,8 +148,43 @@ function createRepo() {
 
   local dataFile=$(tempFile repo-config-json)
   echo "$repoConfigJson" > $dataFile
-  cp $dataFile /tmp/config.json
   _curl $site api/repositories/${repoName} -X PUT -T $dataFile
+}
+
+function isReplicationConfigured() {
+  local site=$1
+  local repoName=$2
+  if _curl $site api/replications/${repoName} 2>/dev/null| jq -Mr '.[].url' | grep -qi "$_EDGE_REPLURL"; then
+    echo "Replication for ${repoName} already exists at ${site}"
+    return 0
+  fi
+  return 1
+}
+
+function createPushReplication() {
+  local site=$1
+  local repoName=$2
+  local remoteSite=$3
+  local remoteRepoName=$4
+  local remoteUserid=$(getConfig $remoteSite userid)
+  local remotePassword=$(getConfig $remoteSite pwd)
+
+  local dataFile=$(tempFile replication.config)
+  cat <<EOF  > $dataFile
+  [
+    {
+      "url": "${_EDGE_REPLURL}/${remoteRepoName}",
+      "username": "${remoteUserid}",
+      "password": "${remotePassword}",
+      "repoKey": "${repoName}",
+      "cronExp": "0 0/15 * * * ?",
+      "enabled": true,
+      "enableEventReplication": true
+    }
+  ]
+EOF
+  _curl $site api/replications/${repoName} -X PUT -T $dataFile
+  echo "Successfully created replication for ${repoName}"
 }
 
 function applyConfig() {
@@ -154,19 +192,49 @@ function applyConfig() {
   local site=$(echo "$configJson" | jq -Mr '.site' )
   local repoName=$(echo "$configJson" | jq -Mr '.repoName' )
   local repoType=$(echo "$configJson" | jq -Mr '.repoType' )
-  local remoteRepoName=$(echo "$configJson" | jq -Mr '.remoteRepoName' )
-  local repoConfigJson=$(echo "$configJson" | jq '.repoConfig + {key: .repoName, rclass: .repoType}')
+  local remoteRepoName=$(echo "$configJson" | jq -Mr '.remoteRepoName? | select (.!=null)' )
+  #local repoConfigJson=$(echo "$configJson" | jq '.repoConfig + {key: .repoName, rclass: .repoType}')
+  local repoConfigJson=$(echo "$configJson" | jq '.repoConfig + {rclass: .repoType}')
+
+
+  echo
+  echo "About to configure $repoName ($repoType) at $site"
 
   if ! isRepoExist $site $repoName; then
     createRepo $site $repoName "$repoConfigJson"
   fi
+
+  if echo "$repoType" | grep -iq "remote"; then
+    [[ ! -z "$remoteRepoName" ]] && echo "Remote repo can have remote replication. Ignored  $remoteRepoName"
+    return 0
+  fi
+
+  if [[ -z "$remoteRepoName" ]]; then
+    echo "Remote repo not configured. Not required ?"
+    return 0
+  fi
+
+  if isReplicationConfigured $site $repoName; then
+    return 0
+  fi
+
+  local remoteSite="edge"
+  if [[ "$site" == "edge" ]]; then
+    remoteSite="master"
+  fi
+
+  if ! isRepoExist $remoteSite $remoteRepoName; then
+    createRepo $remoteSite $remoteRepoName "$repoConfigJson"
+  fi
+
+  createPushReplication $site $repoName $remoteSite $remoteRepoName
 }
 
 init
 
 # Create a temp directory for all files generated during this execution
 MYTMPDIR=$(mktemp -d /tmp/setup.XXXX)
-trap "rm -vrf $MYTMPDIR" EXIT
+trap "rm -rf $MYTMPDIR" EXIT
 
 for repoName in $(cat setupRepo.json | jq -Mr '.[].repoName'); do
   applyConfig  "$(cat setupRepo.json | jq -Mr --arg repo $repoName '.[] | select (.repoName == $repo )')"
